f733a20ab0136fbca5175267c431b4f43fe3a4ec
Aleksi Nokela <n0noal00@students.oamk.fi>
2022-11-26 18:50:46 +0200

    push to repository

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState(0);
+
+  function calculate() {
+    if (weight === 0 || height === 0 || weight === '' || height === '') {
+      alert('Please, input your weight and height')
+    }
+    else {
+      const result = weight.replace(',','.') / (height.replace(',','.') ** 2);
+      setBmi(result.toFixed(1));
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Weight: </Text>
+      <TextInput style={styles.field} keyboardType='numeric' onChangeText={text=>setWeight(text)}/>
+      <Text style={styles.field} >Height: </Text>
+      <TextInput keyboardType='decimal-pad'style={styles.field} onChangeText={text=>setHeight(text)}/>
+      <Text style={styles.field}>BMI: </Text>
+      <Text style={styles.field}>{bmi}</Text>
+      <Button onPress={calculate} title='Calculate BMI'></Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  field: {
+    marginBottom: 10,
   },
 });
